1580273b950a1937bd79f559f9c3eed09e590693
Pan-Grayza <zagraystepan@gmail.com>
2022-08-23 15:26:23 +0100

    login in process

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,13 +3,20 @@ import React from 'react'
 import CreatePost from './CreatePost'
 import Post from './Post'
 
+import {trpc} from '../utils/trpc'
+
 const Main = () => {
+    const {data, error, isLoading} = trpc.useQuery(['hello'])
     return (
         <main className="relative w-full max-w-[32rem] h-max flex flex-col justify-center border-x-2 border-lightBlue-75 divide-y-2 divide-lightBlue-75">
             <div className="fixed w-full sm:w-navWidth top-0 h-[3rem] flex flex-row items-center p-7 py-4 z-50 bg-halfTransperentWhite backdrop-blur-sm">
                 <h1 className="text-xl font-bold">Home</h1>
             </div>
             <CreatePost />
+            {isLoading && <div>Loading...</div>}
+            {error && <div>{JSON.stringify(error)}</div>}
+            {data && <div>{JSON.stringify(data)}</div>}
+
             <Post />
             <Post />
             <Post />
--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -1,14 +1,19 @@
 // src/server/router/index.ts
-import { createRouter } from "./context";
-import superjson from "superjson";
+import {createRouter} from './context'
+import superjson from 'superjson'
 
-import { exampleRouter } from "./example";
-import { protectedExampleRouter } from "./protected-example-router";
+import {postRouter} from './post'
+import {protectedExampleRouter} from './protected-example-router'
 
 export const appRouter = createRouter()
-  .transformer(superjson)
-  .merge("example.", exampleRouter)
-  .merge("question.", protectedExampleRouter);
+    .transformer(superjson)
+    .merge('example.', postRouter)
+    .merge('question.', protectedExampleRouter)
+    .query('hello', {
+        resolve: () => {
+            return 'Hello from trpc server'
+        },
+    })
 
 // export type definition of API
-export type AppRouter = typeof appRouter;
+export type AppRouter = typeof appRouter
